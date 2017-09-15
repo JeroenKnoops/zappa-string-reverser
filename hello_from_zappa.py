89120fcf6ea26cc872398d26ff534ac1564247b8
Jeroen Knoops <jeroen.knoops@gmail.com>
2017-09-15 15:05:13 +0200

    Jungle is massive!

--- hello_from_zappa.py
+++ hello_from_zappa.py
@@ -1,11 +1,16 @@
+import logging
 from flask import Flask
 
 app = Flask(__name__)
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-@app.route('/')
-def hello():
-	return "Hello from zappa! \n"
+@app.route('/', methods=['GET', 'POST'])
+def lambda_handler(event=None, context=None):
+    logger.info('Lambda function invoked index()')
+
+    return 'Jungle is massive!'
 
 if __name__ == '__main__':
-	app.run()
-	
+    app.run(debug=True)
